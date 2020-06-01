ce8c286b723770261e4eeb07dbd3e61071829770
Will B Chang <will.b.chang@gmail.com>
2020-06-01 08:52:49 +0800

    refactor: store ARGV[0] in query

--- search_selection.rb
+++ search_selection.rb
@@ -3,8 +3,9 @@
 require 'uri'
 require 'erb'
 
-urls = URI.extract(ARGV[0]) || []
-lines = ARGV[0].split("\n").reject(&:empty?)[0..4]
+query = ARGV[0]
+urls = URI.extract(query) || []
+lines = query.split("\n").reject(&:empty?)[0..4]
 if urls.empty?
   lines.each do |line|
     system "open 'https://google.com/search?q=#{ERB::Util.url_encode(line)}'"
