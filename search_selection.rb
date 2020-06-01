c3deacd6be598046c9479752f953545d3cc96afa
Will B Chang <will.b.chang@gmail.com>
2020-06-01 08:43:28 +0800

    refactor: replace deprecated method URI.encode() to ERB::Util.url_encode()

--- search_selection.rb
+++ search_selection.rb
@@ -1,10 +1,11 @@
 require "uri"
+require 'erb'
 
 urls = URI.extract(ARGV[0])
 lines = ARGV[0].split("\n").reject(&:empty?)[0..4]
 if urls.empty?
   lines.each { |line|
-    system "open 'https://google.com/search?q=#{URI::encode(line)}'"
+    system "open 'https://google.com/search?q=#{ERB::Util.url_encode(line)}'"
   }
 else
   urls.each { |url| system "open #{url}" }
