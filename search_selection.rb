e87a2d389c9f80239898ce3d77a502058d48362e
Will B Chang <will.b.chang@gmail.com>
2020-06-01 08:35:29 +0800

    refactor: replace multiple line ternary operator with if else

--- search_selection.rb
+++ search_selection.rb
@@ -2,8 +2,10 @@ require "uri"
 
 urls = URI.extract(ARGV[0])
 lines = ARGV[0].split("\n").reject(&:empty?)[0..4]
-urls.length == 0 ?
-    lines.each { |line|     
-        system "open 'https://google.com/search?q=#{URI::encode(line)}'"
-    } :
-    urls.each { |url| system "open #{url}" }
+if urls.length == 0
+  lines.each { |line|
+    system "open 'https://google.com/search?q=#{URI::encode(line)}'"
+  }
+else
+  urls.each { |url| system "open #{url}" }
+end
