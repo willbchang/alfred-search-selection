00d5cd6f1de5af9f1d8047cce5de66a53fb088c2
Will B Chang <will.b.chang@gmail.com>
2020-06-01 08:36:42 +0800

    refactor: replace replace length comparing to empty?

--- search_selection.rb
+++ search_selection.rb
@@ -2,7 +2,7 @@ require "uri"
 
 urls = URI.extract(ARGV[0])
 lines = ARGV[0].split("\n").reject(&:empty?)[0..4]
-if urls.length == 0
+if urls.empty?
   lines.each { |line|
     system "open 'https://google.com/search?q=#{URI::encode(line)}'"
   }
