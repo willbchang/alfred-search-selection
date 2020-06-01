1fe0ebe3a34845e6eb1ea832218890498e30b36e
Will B Chang <will.b.chang@gmail.com>
2020-06-01 08:45:58 +0800

    refactor: avoid using `{...}` for multi-line blocks

--- search_selection.rb
+++ search_selection.rb
@@ -4,9 +4,9 @@ require 'erb'
 urls = URI.extract(ARGV[0])
 lines = ARGV[0].split("\n").reject(&:empty?)[0..4]
 if urls.empty?
-  lines.each { |line|
+  lines.each do |line|
     system "open 'https://google.com/search?q=#{ERB::Util.url_encode(line)}'"
-  }
+  end
 else
   urls.each { |url| system "open #{url}" }
 end
